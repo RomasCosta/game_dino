38f95923ce4778e186777973cd82497b448505d2
RomasCosta <romariocosta25@outlook.com>
2022-05-07 20:23:39 -0300

    Criando jogo dino sem net HTM, CSS e JS

---
+++ script.js
@@ -0,0 +1,67 @@
+const dino = document.querySelector('.dino')
+const background = document.querySelector('.background')
+let isjumping = false
+let position = 0
+
+function handleyKeyUp(event) {
+  if(event.keyCode === 32) {
+    if(!isjumping) { 
+    jump();  
+    } 
+  }
+}
+
+function jump() {
+  isjumping = true
+
+  let upInterval = setInterval(() => {
+    if(position >= 150) {
+      clearInterval(upInterval);
+      //Descendo
+      let downInterval = setInterval(() => {
+        if(position <= 0) {
+          clearInterval(downInterval)
+          isjumping = false
+        } else { 
+        position -= 20
+        dino.style.bottom = position + 'px';
+       }
+      }, 20)
+    } else {
+    //subir-pular
+    position += 20
+
+    dino.style.bottom = position + 'px';
+     }
+  }, 20)
+}
+
+function createCactus() {
+  const cactus = document.createElement('div');
+  let cactusPosition = 1000;
+  let randowTime = Math.random() * 6000;
+
+  cactus.classList.add('cactus')
+  cactus.style.left = 1000 + 'px'
+  background.appendChild(cactus)
+
+  let leftInterval = setInterval(() => {
+    if(cactusPosition < -60) {
+      clearInterval(leftInterval)
+      background.removeChild(cactus)
+    } else if(cactusPosition > 0 && cactusPosition < 60 && position < 60) {
+    //Game Over
+      clearInterval(leftInterval);
+      document.body.innerHTML = '<h1 class="game-over">Fim de Jogo!</h1>'
+    }else { 
+    cactusPosition -= 10;
+    cactus.style.left = cactusPosition + 'px'
+    }
+  }, 20)
+
+  setTimeout(createCactus, randowTime)
+
+}
+
+createCactus();
+document.addEventListener('keyup', handleyKeyUp)
